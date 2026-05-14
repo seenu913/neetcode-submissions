c1d60bdf024a8604d70adc5d0e0461926f02f097
seenu913 <44984412+seenu913@users.noreply.github.com>
2026-05-13 22:45:43 -0500

    Add: validate-parentheses - submission-5

---
+++ Data Structures & Algorithms/validate-parentheses/submission-5.java
@@ -0,0 +1,18 @@
+class Solution {
+    public boolean isValid(String s) {
+        Deque stack = new ArrayDeque<>();
+        Map<Character,Character> charMap = new HashMap<>();
+        charMap.put(')','(');
+        charMap.put('}','{');
+        charMap.put(']','[');
+        for(char c : s.toCharArray()) {
+            if(charMap.containsKey(c)) {
+                char ct = charMap.get(c);
+                if(stack.isEmpty() || !stack.pop().equals(ct)) return false;
+            } else {
+                stack.push(c);
+            }
+        }
+        return stack.isEmpty();
+    }
+}
